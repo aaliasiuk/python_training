191a7174d5872fb88418fb76c33a3606ae53f825
aaliasiuk <aliasiuk13@gmail.com>
2018-12-28 13:36:52 -0800

    Created login and logout as a separate functions

--- LoginTest.py
+++ LoginTest.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support.ui import Select
+#from selenium.common.exceptions import NoSuchElementException
+#from selenium.common.exceptions import NoAlertPresentException
+import unittest
 
 class LoginTest(unittest.TestCase):
     def setUp(self):
@@ -17,37 +17,47 @@ class LoginTest(unittest.TestCase):
     
     def test_login(self):
         driver = self.driver
-        driver.get("https://buy.gazellestaging.com/")
+        self.open_home_page(driver)
+        self.login(driver)
+        self.logout(driver)
+       
+    def logout(self, driver):
+        driver.find_element_by_id("customer_logout_link").click()
+
+    def login(self, driver):
         driver.find_element_by_id("customer_login_link").click()
         driver.find_element_by_id("CustomerEmail").clear()
         driver.find_element_by_id("CustomerEmail").send_keys("aaliasiuk@gazelle.com")
         driver.find_element_by_id("CustomerPassword").click()
         driver.find_element_by_id("CustomerPassword").clear()
         driver.find_element_by_id("CustomerPassword").send_keys("Sqa2018sqa")
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::input[2]").click()
-        driver.find_element_by_id("customer_logout_link").click()
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::input[2]").click()
+
+    def open_home_page(self, driver):
+        driver.get("https://buy.gazellestaging.com/")
+
+    # def is_element_present(self, how, what):
+    #     try: self.driver.find_element(by=how, value=what)
+    #     except NoSuchElementException as e: return False
+    #     return True
+    #
+    # def is_alert_present(self):
+    #     try: self.driver.switch_to_alert()
+    #     except NoAlertPresentException as e: return False
+    #     return True
+    #
+    # def close_alert_and_get_its_text(self):
+    #     try:
+    #         alert = self.driver.switch_to_alert()
+    #         alert_text = alert.text
+    #         if self.accept_next_alert:
+    #             alert.accept()
+    #         else:
+    #             alert.dismiss()
+    #         return alert_text
+    #     finally: self.accept_next_alert = True
+    #
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
